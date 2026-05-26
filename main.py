2ad7ed9c66daa6e4e62861cd32deb57257042c4e
DaffyTheDuck <34976093+DaffyTheDuck@users.noreply.github.com>
2026-05-27 01:23:12 +0530

    fix CORS for production

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",        # local Next.js dev
         "https://*.vercel.app",         # any Vercel deployment
+        "https://pricesense-frontend.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],                # allow GET, POST, PUT, DELETE etc.
